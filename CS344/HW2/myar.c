9c7e3cae97e149e7c05e599fe9b002ba8d621b9b
Vladis466 <predovic900@gmail.com>
2015-07-07 23:22:37 -0700

    permissions file found pg.296 linux Prog Inter thank god

--- CS344/HW2/myar.c
+++ CS344/HW2/myar.c
@@ -58,7 +58,22 @@
 
 //returns a file 
 
-
+char * /* Return ls(1)-style string for file permissions mask */
+filePermStr(mode_t perm, int flags)
+{
+static char str[STR_SIZE];
+snprintf(str, STR_SIZE, "%c%c%c%c%c%c%c%c%c",
+(perm & S_IRUSR) ? 'r' : '-', (perm & S_IWUSR) ? 'w' : '-', (perm & S_IXUSR) ?
+(((perm & S_ISUID) && (flags & FP_SPECIAL)) ? 's' : 'x') :
+(((perm & S_ISUID) && (flags & FP_SPECIAL)) ? 'S' : '-'),
+(perm & S_IRGRP) ? 'r' : '-', (perm & S_IWGRP) ? 'w' : '-', (perm & S_IXGRP) ?
+(((perm & S_ISGID) && (flags & FP_SPECIAL)) ? 's' : 'x') :
+(((perm & S_ISGID) && (flags & FP_SPECIAL)) ? 'S' : '-'),
+(perm & S_IROTH) ? 'r' : '-', (perm & S_IWOTH) ? 'w' : '-',(perm & S_IXOTH) ?
+(((perm & S_ISVTX) && (flags & FP_SPECIAL)) ? 't' : 'x') :
+(((perm & S_ISVTX) && (flags & FP_SPECIAL)) ? 'T' : '-'));
+return str;
+}
 
 
 void appendFiles(const char *archive, const char *files[], int num_files);
