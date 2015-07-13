ea0c4f8e1c34e814ec9f344644bbaed64d48d6d2
Vladis466 <predovic900@gmail.com>
2015-07-13 16:37:48 -0700

    Updated brought over from using file. Defined permissions

--- CS344/HW2/myar.h
+++ CS344/HW2/myar.h
@@ -10,15 +10,15 @@
 //void ToC(const char *Chive, int verbosity);
 
 #define SATANPERMS S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
+#define AR_HDR struct ar_hdr
+#define AR_HSZ sizeof(AR_HDR)
 
+char *trigger;
 
 char *PermissionFiles(mode_t perm);
 void appendFiles(const char *archive, const char *files[], int num_files);
-void buildHeader(struct ar_hdr *hdr, const struct stat *st, char *fName);
+void formatHeader(struct ar_hdr *hdr, const struct stat *st, char *fName);
 void fHeader(const char *file, const struct stat *st, struct ar_hdr *hdr);
 void wrFileAr(int archive, int fd, struct stat *st);
 void appALL(const char *archive);
 void deleteFiles(const char *Ar, const char **delFiles ,int i);
-
-void print_archive(const char *archive, int verbs);
-void print_hdr(struct ar_hdr *hdr);
